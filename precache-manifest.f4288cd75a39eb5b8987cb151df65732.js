a35e393f80dff0f25a8e68df433553f93e14595b
Landon Wark <lando.wark@gmail.com>
2020-06-03 15:18:02 -0500

    Updates

---
+++ precache-manifest.f4288cd75a39eb5b8987cb151df65732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c5d32038aeb18db1041636599b0c85",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "696bbb7714df1cc8f072",
+    "url": "/simple-todo-app/static/css/main.2efa2c39.chunk.css"
+  },
+  {
+    "revision": "0139ce81d585db9a214e",
+    "url": "/simple-todo-app/static/js/2.b9476618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.b9476618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696bbb7714df1cc8f072",
+    "url": "/simple-todo-app/static/js/main.e1ef95c4.chunk.js"
+  },
+  {
+    "revision": "e165d461a30ce2094b0e",
+    "url": "/simple-todo-app/static/js/runtime-main.065a362d.js"
+  }
+]);
